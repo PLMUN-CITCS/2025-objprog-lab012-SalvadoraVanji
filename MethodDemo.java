b8c0bb6e20e740b617d18c917f0a88b801050b12
vanji <salvadorajovan_bscs@plmun.edu.ph>
2025-02-21 21:45:20 +0800

    Submitting OBJPROG Week 05 - Laboratory # 12

---
+++ MethodDemo.java
@@ -0,0 +1,14 @@
+public class MethodDemo {
+    public static void main(String[] args) {
+        greet();
+        printMessage("Hello from printMessage method!");
+    }
+
+    public static void greet() {
+        System.out.println("Hello from the greet method!");
+    }
+
+    public static void printMessage(String message) {
+        System.out.println(message);
+    }
+}
